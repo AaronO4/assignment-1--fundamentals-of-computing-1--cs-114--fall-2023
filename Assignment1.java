71ae5dc7aacfcf22d578a86e1c41311dc3f8ca89
AaronO4 <soliver@hartford.edu>
2023-09-18 16:01:58 -0400

    Priints Initials in ascii
    AO in biiiig letters

--- Assignment1.java
+++ Assignment1.java
@@ -3,7 +3,11 @@
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
-    System.out.println("Ascii code goes here");
-
+System.out.println("     AAA         OOOOOOOOOO");
+System.out.println("    AA AA       OOO      OOO");
+System.out.println("   AA   AA      OO        OO");
+System.out.println("  AAAAAAAAA     OO        OO");
+System.out.println(" AA       AA    OOO      OOO");
+System.out.println("AA         AA    OOOOOOOOOO");
   }
 }
