6c0d77627c7fe4f9ef2b56f31d8b568a010a1459
AaronO4 <soliver@hartford.edu>
2023-09-18 16:40:34 -0400

    messing with tempurature
    framework

--- Assignment1.java
+++ Assignment1.java
@@ -9,5 +9,13 @@ System.out.println("   AA   AA      OO        OO");
 System.out.println("  AAAAAAAAA     OO        OO");
 System.out.println(" AA       AA    OOO      OOO");
 System.out.println("AA         AA    OOOOOOOOOO");
+
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter a temperature in Fahrenheit:");
+int temperature = scan.nextLine(); // taking input
+float makeCel = (5.0/9.0);
+float temp = ((temperature - 32) * makeCel);
+System.out.println(temp);
   }
 }
