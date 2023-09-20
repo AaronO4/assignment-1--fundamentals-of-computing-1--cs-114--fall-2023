45dd48d77e24081868dc6416324a9ba7771a0fa2
AaronO4 <soliver@hartford.edu>
2023-09-20 16:33:20 -0400

    dice roller
    functions in its entirety! theoretically!

--- Assignment1.java
+++ Assignment1.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+
+//variable graveyard
+double temperature;
+double makeCel = (.556);
+int consReduction = 32;
+double temp;
+double tempFinal;
+final int MAX = 16352; //16384 total, 32 subtracted from Min
+int faceValue;
+
+//code starts here
 System.out.println("     AAA         OOOOOOOOOO");
 System.out.println("    AA AA       OOO      OOO");
 System.out.println("   AA   AA      OO        OO");
@@ -11,19 +21,23 @@ System.out.println("  AAAAAAAAA     OO        OO");
 System.out.println(" AA       AA    OOO      OOO");
 System.out.println("AA         AA    OOOOOOOOOO");
 
-int temperature;
-double makeCel = (.5556);
-int consReduction = 32;
-double temp;
-double tempFinal;
 Scanner scan = new Scanner(System.in);
 
 System.out.println("Enter a temperature in Fahrenheit:");
 temperature = scan.nextInt();
 temp = (temperature - consReduction);
 tempFinal = temp * makeCel;
-System.out.println(tempFinal);
-scan.close();
+System.out.println((int)tempFinal);
+
+
+// put string assignment here
+String fiveLetterString = scan.nextLine();
+
+scan.close(); // this goes at the very very end of scan functions.
+
+//rolls dice
+  faceValue = (int)(Math.random() * MAX) + 32; 
+  System.out.println(faceValue);
 
   }
 }
